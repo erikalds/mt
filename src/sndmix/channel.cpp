3c46ff28378d04107b040be3abd812f134cb0f5f
erikalds <erikalds@gmail.com>
2021-11-14 12:20:33 +0100

    Implemented Channel::put_data

--- src/sndmix/channel.cpp
+++ src/sndmix/channel.cpp
@@ -1,7 +1,29 @@
 #include "channel.h"
 
+#include "sound.h"
+
 namespace mt::snd {
 
+void Channel::put_data(std::span<float> output_buffer,
+                       const PaStreamCallbackTimeInfo& /*time_info*/,
+                       std::size_t num_output_channels)
+{
+  if (!playing_sound
+      || !playing_sound->first)
+  {
+    return;
+  }
 
+  const int sample_rate{44100};
+  playing_sound->first = playing_sound->second.write_to(output_buffer,
+                                                        num_output_channels,
+                                                        sample_rate,
+                                                        *playing_sound->first);
+}
+
+void Channel::play(Sound s)
+{
+  playing_sound = std::make_pair(0, s);
+}
 
 }  // namespace mt::snd
--- src/sndmix/channel.h
+++ src/sndmix/channel.h
@@ -1,16 +1,24 @@
 #ifndef CHANNEL_H
 #define CHANNEL_H
 
+#include "sndmix/audiochannel.h"
 #include "sndmix/sourcenode.h"
+#include "sndmix/sound.h"
 
 namespace mt::snd {
 
-class Channel : public SourceNode
+class Channel : public SourceNode,
+                public AudioChannel
 {
 public:
   void put_data(std::span<float> output_buffer,
                 const PaStreamCallbackTimeInfo& time_info,
                 std::size_t num_output_channels) override;
+
+  void play(Sound s) override;
+
+private:
+  std::optional<std::pair<std::optional<std::size_t>, Sound>> playing_sound;
 };
 
 }  // namespace mt::snd
